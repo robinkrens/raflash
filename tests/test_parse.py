48527dafe9e6df1ac0c4c0a93feaf386da7fa6a4
Robin Krens <robin@robinkrens.nl>
2024-02-10 14:43:38 +0100

    test: added test for unpack packets

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,8 +1,13 @@
-from flasher.flasher import calc_sum
+from flasher.flasher import calc_sum, unpack_pkt
 import pytest
 
 def test_calc_sum():
     assert calc_sum(0x12, ['0x00']) == (0, 0x2, 0xEC)
     assert calc_sum(0x34, ['0x00']) == (0, 0x2, 0xCA)
     assert calc_sum(0x00, ['0x00']) == (0, 0x02, 0xFE)
+
+def test_unpack():
+    assert unpack_pkt(b'\x81\x00\x02\x00\x00\xFE\x03') == ['0x00']
+    assert unpack_pkt(b'\x81\x00\x02\x12\x00\xEC\x03') == ['0x00']
+    assert unpack_pkt(b'\x81\x00\x02\x13\x00\xEB\x03') == ['0x00']
 
