ba35688e1eaf338984b79b0280424e766c09b199
Robin Krens <robin@robinkrens.nl>
2024-02-15 11:05:45 +0100

    setup.py: change name and add entry point

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "renesas-ra-flasher",
+    name = "raflash",
     version = "0.0.1",
     author = "Robin Krens",
     description = ("Flasher for the built in ROM bootloader for Renesas RA microcontrollers"),
     license = "GNU",
     keywords = "Renesas RA chipset flasher",
     packages=['src', 'tests'],
+    entry_points={
+        'console_scripts': [
+            'raflash = src.RAFlasher:main',
+        ],
+    },
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 1",
