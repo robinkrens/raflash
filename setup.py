09aadf254ef8e77335da66cdd7b8f1b77e17453f
Robin Krens <robin@robinkrens.nl>
2024-02-15 10:32:22 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -2,22 +2,18 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "ra-flasher",
+    name = "renesas-ra-flasher",
     version = "0.0.1",
     author = "Robin Krens",
-    description = ("An example of how to set up pytest"),
+    description = ("Flasher for the built in ROM bootloader for Renesas RA microcontrollers"),
     license = "GNU",
     keywords = "Renesas RA chipset flasher",
     packages=['src', 'tests'],
-    #long_description=read('README.md'),
+    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 1",
     ],
