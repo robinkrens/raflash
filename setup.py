18f8db21b188a6104d47d52fee6dd2ec2ce5cb69
Robin Krens <robin@robinkrens.nl>
2024-02-15 12:13:12 +0100

    setup.py: add package reqs

--- setup.py
+++ setup.py
@@ -6,13 +6,26 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "raflash",
-    version = "0.0.1",
-    author = "Robin Krens",
-    description = ("Flasher for the built in ROM bootloader for Renesas RA microcontrollers"),
-    license = "GNU",
-    keywords = "Renesas RA chipset flasher",
+    name="raflash",
+    version="0.0.1",
+    author="Robin Krens",
+    description=("Flasher for the built in ROM bootloader for Renesas RA microcontrollers"),
+    license="GNU",
+    keywords="Renesas RA chipset flasher",
     packages=['src', 'tests'],
+    install_requires=[
+        'exceptiongroup>=1.2.0',
+        'future>=0.18.3',
+        'iniconfig>=2.0.0',
+        'iso8601>=2.1.0',
+        'packaging>=23.2',
+        'pluggy>=1.4.0',
+        'pytest>=8.0.0',
+        'pyusb>=1.2.1',
+        'PyYAML>=6.0.1',
+        'tomli>=2.0.1',
+        'tqdm>=4.66.2',
+    ],
     entry_points={
         'console_scripts': [
             'raflash = src.RAFlasher:main',
